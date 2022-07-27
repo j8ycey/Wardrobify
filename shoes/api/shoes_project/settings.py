fa2832c32574d1e3862a3f888c94b7f226a0cfef
Jonathan <yoo.y.jonathan@gmail.com>
2022-07-27 14:16:08 -0700

    test

--- shoes/api/shoes_project/settings.py
+++ shoes/api/shoes_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "corsheaders",
+    "shoes_rest.apps.ShoesRestConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Shoe(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=50)
+  picture_url = models.URLField()
+  bin = models.ForeignKey(
+    "BinVO",
+    related_name="bins",
+    
+  )
